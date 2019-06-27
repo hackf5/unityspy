f3babeb68dc28a14c83a42ec81ae2b580f8f26d9
Brian Tyler <brian.tyler@gmail.com>
2019-06-27 16:52:13 +0100

    Fixed typo.

--- src/HackF5.UnitySpy/Detail/TypeDefinition.cs
+++ src/HackF5.UnitySpy/Detail/TypeDefinition.cs
@@ -13,7 +13,7 @@
     /// <summary>
     /// Represents an unmanaged _MonoClass instance in a Mono process. This object describes the type of a class or
     /// struct. The .NET equivalent is <see cref="System.Type"/>.
-    /// See: _MonoImage in https://github.com/Unity-Technologies/mono/blob/unity-master/mono/metadata/class-internals.h.
+    /// See: _MonoClass in https://github.com/Unity-Technologies/mono/blob/unity-master/mono/metadata/class-internals.h.
     /// </summary>
     [PublicAPI]
     [DebuggerDisplay("Class: {" + nameof(TypeDefinition.Name) + "}")]
