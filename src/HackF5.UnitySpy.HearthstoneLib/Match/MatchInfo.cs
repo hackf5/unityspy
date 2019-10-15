bd9efa41291d11af9c719f109bf02c7164a3911d
sebastientromp <sebastien.tromp@gmail.com>
2019-10-15 13:51:39 +0200

    add support for GetMatchInfo API

---
+++ src/HackF5.UnitySpy.HearthstoneLib/Match/MatchInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace HackF5.UnitySpy.HearthstoneLib.Match
+{
+    public class MatchInfo
+    {
+        public Player LocalPlayer { get; set; }
+
+        public Player OpposingPlayer { get; set; }
+
+        public int BrawlSeasonId { get; set; }
+
+        public int MissionId { get; set; }
+
+        public int RankedSeasonId { get; set; }
+
+        public int GameType { get; set; }
+
+        public int FormatType { get; set; }
+
+        public bool Spectator { get; set; }        
+    }
+}
